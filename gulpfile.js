dc9266e6e63817283945e3ca7c5d599975f7a35f
rdemello <demez884@hotmail.com>
2017-07-29 23:15:08 +0100

    aboutme

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('css/*.scss')
+    gulp.src('assets/css/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(function(f) {
             return f.base;
