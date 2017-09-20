323fe585a6132b58112fafda5075d1c09dca1888
Ryan Work <demez884@hotmail.com>
2017-09-20 17:20:05 +0100

    menu

--- assets/scripts/sprout.js
+++ assets/scripts/sprout.js
@@ -1,9 +1,9 @@
 $(window).scroll(function() {
     if ($(window).scrollTop() > 300) {
-        $('.tracker').css('opacity',1)
+        $('.menu').css('opacity',1)
     }
     else {
-        $('.tracker').css('opacity',0)
+        $('.menu').css('opacity',0)
     }
 });
 
@@ -12,8 +12,8 @@ $(document).ready(function(){
     // color(39, 130, 62,10);
 
     setColor(127,176,73);
-    color(127,176,73,8)
-    
+    color(127,176,73,8);
+
     colourChange();
     window.setInterval(function(){
         colourChange();
