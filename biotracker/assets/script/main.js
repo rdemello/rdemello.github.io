8bb8009d08b20641dd2018e3f829395b3cb465b5
Ryan Work <demez884@hotmail.com>
2018-01-30 15:17:41 +0000

    biotracker

--- biotracker/assets/script/main.js
+++ biotracker/assets/script/main.js
@@ -1,4 +1,6 @@
 var rawData;
+var bpChart;
+
 
 var cleanedData =[];
 var sysData =[];
@@ -14,6 +16,7 @@ var creatineData =[];
 var proteinData =[];
 var biotinData =[];
 var multiVitData =[];
+var dateData =[];
 
 const $key = "1_iwxs8PvmZl01-oBlQWSDS0SRSfjsSWZbfB5Om2rovY",
     $sheetNum = 1,
@@ -59,9 +62,9 @@ function checkSheet(){
 }
 
 function sortData(){
-
     for(i=1;i<rawData.length;i++){
         if(rawData[i].length>15){
+            dateData.push(rawData[i][0])
             sysData.push(parseInt(rawData[i][3]))
             diaData.push(parseInt(rawData[i][4]))
             bpmData.push(parseInt(rawData[i][5]))
@@ -79,19 +82,77 @@ function sortData(){
     }
 }
 
+function evc(x){
+    if(x==null){
+        return 0
+    } else {
+        return x
+    }
+}
 
-checkSheet();
-
+function threeDayAverageCalc(array){
+    return Math.round((evc(array[array.length-1]) + evc(array[array.length-2]) + evc(array[array.length-3]))/3)
+}
 
 function loadUpData(){
-
-    var currentWeight = weightData[weightData.length-1];
-    document.getElementById("currentWeight").innerHTML = currentWeight
-
     var latestCoffee = caffeineData.length - caffeineData.lastIndexOf(1) 
     document.getElementById("latestCoffee").innerHTML = latestCoffee
 
     var latestAlcohol = alcoholData.length - alcoholData.lastIndexOf(1) 
     document.getElementById("latestAlcohol").innerHTML = latestAlcohol
 
+    threeDayAverage();
 }
+
+function threeDayAverage(){
+    document.getElementById("3dayWeightlb").innerHTML = threeDayAverageCalc(weightData)
+    document.getElementById("3dayWeightkg").innerHTML = Math.round( (threeDayAverageCalc(weightData) * 0.453) * 10 ) / 10; 
+
+    document.getElementById("3daySys").innerHTML = threeDayAverageCalc(sysData)
+    document.getElementById("3dayDia").innerHTML = threeDayAverageCalc(diaData)
+    document.getElementById("3dayVO2").innerHTML = threeDayAverageCalc(VO2Data)
+}
+
+function loadChart(){
+    var ctx = document.getElementById('bpChart').getContext('2d');
+        bpChart = new Chart(ctx, {
+        type: 'line',
+
+        data: {
+            labels: dateData,
+            datasets: [{
+                label: "Systolic",
+                fill: false,
+                borderColor: 'rgb(255, 99, 132)',
+                data: sysData,
+            },{
+                label: "Diastolic",
+                fill: false,
+                borderColor: 'rgb(109, 164, 232)',
+                data: diaData,
+            }]
+        },
+
+        options: {
+            legend: {
+                display:false
+            },
+            tooltips:{
+                mode:'index',
+                axis: 'x',
+                intersect: false
+            }
+        }
+    });
+}
+
+function updateChart(){
+    bpChart.update();
+    bpChart.resize();
+}
+
+$(document).ready(function(){
+    checkSheet();
+    loadChart();
+    // updateChart();
+})
