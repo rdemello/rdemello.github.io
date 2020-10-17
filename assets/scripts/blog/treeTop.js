fb10d7f5c712e51a92154e3b63b05a7888a9600c
rdemello <demez884@hotmail.com>
2020-10-17 23:06:35 +0100

    TreeTopWalk

---
+++ assets/scripts/blog/treeTop.js
@@ -0,0 +1,3 @@
+$('.headerPage').parallax({imageSrc: '../../assets/media/blog/trees.jpg'});
+$('#dam').parallax({imageSrc: '../../assets/media/blog/week1/threeGorges.jpg'});
+$('#end').parallax({imageSrc: '../../assets/media/blog/treeTop/landscape.jpg'});
