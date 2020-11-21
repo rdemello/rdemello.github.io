7970a828d5a9cc2d433c0908b231b4abb020b4dd
rdemello <demez884@hotmail.com>
2020-11-21 17:46:20 +0000

    biodiversity

---
+++ assets/scripts/blog/biodiversity.js
@@ -0,0 +1,2 @@
+$('.headerPage').parallax({imageSrc: '../../assets/media/blog/deforestation/deforestation.jpg'});
+$('#monoculture').parallax({imageSrc: '../../assets/media/blog/biodiversity/monoculture.jpg'});
